6470f0d5391da1c4fcbf9b4da298a2da7f7a108e
speedtypingtest1 <147539449+speedtypingtest1@users.noreply.github.com>
2024-05-23 22:36:52 +0800

    Update paragraphs.js

--- js/paragraphs.js
+++ js/paragraphs.js
@@ -1,4 +1,4 @@
 const paragraphs = [
-    "The big brown fox jumps over the lazy dog. Pack my box with five dozen liquor jugs. Jackdaws love my big sphinx of quartz. Mr. Jock, TV quiz PhD, bags few lynx.",
+    "The big brown fox jumps over the lazy dog. Pack my box with five dozen liquor jugs. Jackdaws love my big sphinx of quartz. Mr. Jock, TV quiz PhD, bags few lynx. Quick zephyrs blow, vexing daft Jim. How razorback-jumping frogs can level six piqued gymnasts! Bright vixens jump; dozy fowl quack. Lazy John was fixing big, opaque, quaint red zebra's yellow jar..",
    
 ];
